198c837513841cf6c3af89ab1bf4003f52d3f24d
snowboi <snowboi@gena.so>
2013-07-11 21:24:19 +0900

    image test

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import os
 import urllib
 
 from google.appengine.api import users
-from google.appengine.ext import ndb
+from google.appengine.ext import db
 
 import jinja2
 import webapp2
@@ -21,24 +21,40 @@ DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return ndb.Key('Guestbook', guestbook_name)
+    return db.Key.from_path('Guestbook', guestbook_name or 'default_guest')
 
+"""Models an individual Guestbook entry with author, content, and date."""
+class Greeting(db.Model):
+    author = db.StringProperty()
+    content = db.StringProperty(multiline=True)
+    avatar = db.BlobProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
-class Greeting(ndb.Model):
-    """Models an individual Guestbook entry with author, content, and date."""
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
+class Image(webapp2.RequestHandler):
+    def get(self):
+        greeting = db.get(self.request.get('img_id'))
+        if greeting.avatar:
+            self.response.headers['Content-Type'] = 'imgage/png'
+            self.response.out.write(greeting.avatar)
+        else:
+            self.error(404)
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         guestbook_name = self.request.get('guestbook_name',
                                           DEFAULT_GUESTBOOK_NAME)
+        '''
         greetings_query = Greeting.query(
             ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
+        '''
+        greetings = db.GqlQuery('SELECT * '
+                                'FROM Greeting '
+                                'WHERE ANCESTOR IS :1 '
+                                'ORDER BY date DESC LIMIT 10',
+                                guestbook_key(guestbook_name))
+
 
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -58,6 +74,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
+
 class Guestbook(webapp2.RequestHandler):
 
     def post(self):
@@ -70,8 +87,11 @@ class Guestbook(webapp2.RequestHandler):
         greeting = Greeting(parent=guestbook_key(guestbook_name))
 
         if users.get_current_user():
-            greeting.author = users.get_current_user()
+            greeting.author = users.get_current_user().nickname()
 
+        avatar = self.request.get('img')
+        if avatar:
+            greeting.avatar = db.Blob(avatar)
         greeting.content = self.request.get('content')
         greeting.put()
 
@@ -82,4 +102,5 @@ class Guestbook(webapp2.RequestHandler):
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sign', Guestbook),
+    ('/img', Image),
 ], debug=True)
